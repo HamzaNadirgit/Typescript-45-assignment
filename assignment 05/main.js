2ce7828153be781c20c2aa67a5d5c11029d511c3
HAMZA NADIR <hamzaofficial272@gmail.com>
2024-03-30 16:15:26 +0500

    first commit

---
+++ assignment 05/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//assignment no 5
+let famousperson = "Albert Einstein";
+let famQoute = "A person who never made a mistake never tried anything new.";
+let message = `${famousperson} once said, "${famQoute}}"`;
+console.log(message);
---
+++ assignment 05/main.ts
@@ -0,0 +1,6 @@
+//assignment no 5
+let famousperson:string = "Albert Einstein";
+let famQoute:string = "A person who never made a mistake never tried anything new.";
+let message:string = `${famousperson} once said, "${famQoute}}"`;
+
+console.log(message);
